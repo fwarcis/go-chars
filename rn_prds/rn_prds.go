7cec69a02aecfe711569f297656bcb784d7cb82d
fwarcis <fwarcis@gmail.com>
2026-07-11 02:58:29 +0000

    refact: Rename 'rns' package to 'rn_prds'

---
+++ rn_prds/rn_prds.go
@@ -0,0 +1,58 @@
+package rn_prds
+
+import (
+	"iter"
+	"strings"
+
+	chrs "github.com/fwarcis/go-chars"
+)
+
+type Pred = func(rune) bool
+
+func Eq(left rune) func(right rune) bool {
+	return func(right rune) bool {
+		return left == right
+	}
+}
+
+func Nx[C chrs.Chars](comparing C) Pred {
+	pos := 0
+	runes := []rune(string(comparing))
+	return func(rn rune) bool {
+		if pos == len(runes) {
+			return false
+		}
+		equals := runes[pos] == rn
+		pos++
+		return equals
+	}
+}
+
+func In[C chrs.Chars](comparing C) Pred {
+	return func(rn rune) bool {
+		return strings.ContainsRune(
+			string(comparing), rn)
+	}
+}
+
+func It[C rune | chrs.Chars](iterating C) iter.Seq[rune] {
+	runes := []rune(string(iterating))
+	return func(yield Pred) {
+		for _, r := range runes {
+			if !yield(r) {
+				return
+			}
+		}
+	}
+}
+
+func All[C rune | chrs.Chars](iterating C) iter.Seq2[int, rune] {
+	runes := []rune(string(iterating))
+	return func(yield func(int, rune) bool) {
+		for i, r := range runes {
+			if !yield(i, r) {
+				return
+			}
+		}
+	}
+}
